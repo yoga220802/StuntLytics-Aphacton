f8ad2019aca34e2f691285acc2f75b4faa286c14
yoga220802 <yogaagustiansyah01@gmail.com>
2025-09-25 11:57:00 +0700

    fix(config): update Elasticsearch URL from 127.0.0.1 to localhost

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # --- Konfigurasi Koneksi Elasticsearch ---
-ES_URL = os.getenv("ES_URL", "http://127.0.0.1:9200")
+ES_URL = os.getenv("ES_URL", "http://localhost:9200")
 STUNTING_INDEX = os.getenv("STUNTING_INDEX", "stunting-data")
 BALITA_INDEX = os.getenv("BALITA_INDEX", "jabar-balita-desa")
 NUTRITION_INDEX = os.getenv("NUTRITION_INDEX", "jabar-tenaga-gizi")
